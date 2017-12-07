3747848627affbf8da7d1792ebb10b6f885e2199
Shayne <skshayne@gmail.com>
2017-12-07 01:22:36 +0000

    Latest monitoring update

--- snoozebud.py
+++ snoozebud.py
@@ -12,7 +12,6 @@ spi.open(0,0)
 
 # Setup sensor and motor pins
 motorPin = 17
-inputPin = 18
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(motorPin, GPIO.OUT)
 GPIO.output(motorPin, GPIO.LOW)
@@ -24,15 +23,38 @@ def read_adc(adc_num):
 
 def monitorForTime():
     timeout = time.time() + DELAY_SEC
-    max_reading = 0
+    max_reading_1 = 0
+    max_reading_2 = 0
+    min_reading_1 = 1000
+    min_reading_2 = 1000
+    
     while True:
-        time.sleep(0.1)
-        reading = read_adc(0)
-        print reading
-        if reading > max_reading:
-            max_reading = reading
+        time.sleep(0.2)
+        reading_1 = read_adc(1)
+        #print("1 {0}".format(reading_1))
+        reading_2 = read_adc(2)
+        #print("2 {0}".format(reading_2))
+        #print reading
+
+        # reading 1
+        if reading_1 > max_reading_1:
+            max_reading_1 = reading_1
+        elif reading_1 < min_reading_1:
+            min_reading_1 = reading_1
+
+        # reading 2
+        if reading_2 > max_reading_2:
+            max_reading_2 = reading_2
+        elif reading_2 < min_reading_2:
+            min_reading_2 = reading_2
+            
         if time.time() > timeout:
-            return max_reading
+            print("Sensor 1 - Max reading was {0}, min reading was {1}".format(max_reading_1, min_reading_1))
+            print("Sensor 2 - Max reading was {0}, min reading was {1}".format(max_reading_2, min_reading_2))
+            print("Difference 1: {0}".format(max_reading_1-min_reading_1))
+            print("Difference 2: {0}".format(max_reading_2-min_reading_2))
+            print("")
+            return max_reading_1
 
 def noMovementForTime():
     print("Starting to monitor")
@@ -40,7 +62,7 @@ def noMovementForTime():
     while True:
         max_reading = monitorForTime();
         if max_reading > SENSITIVITY:
-            print("Movement detected, monitoring done. Max reading was {0}".format(max_reading))
+            print("Movement detected, monitoring done.")
             #print(max_reading)
             return False
         else:
@@ -73,12 +95,12 @@ def vibrateForTime(seconds):
 
 def main():
     while True:
-        print("Voltage level: {0}".format(read_adc(1)))
-        time.sleep(0.2)
-        #noMovement = noMovementForTime()
-        #if noMovement == True:
-        #    vibrateForTime(3)
-	#    time.sleep(1)
+        #print("Voltage level: {0}".format(read_adc(1)))
+        #time.sleep(0.2)
+        noMovement = noMovementForTime()
+        if noMovement == True:
+            vibrateForTime(3)
+	    time.sleep(1)
 
 try:
     main()
